e17afe4edca0ffd898e0480564b739cd43950e70
ktjybl <64632527+ktjybl@users.noreply.github.com>
2021-06-11 17:22:52 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+﻿const btnMenu=document.querySelector('.menu-button');
+const closeMenu=document.querySelector('.menu-close');
+const menuElem=document.querySelector('.nav-menu');
+
+const showMenu=()=>{
+        menuElem.classList.add('is-active');
+       closeMenu.classList.add('is-active');
+};
+
+const hideMenu=()=>{
+        menuElem.classList.remove('is-active');
+       closeMenu.classList.remove('is-active');
+};
+
+btnMenu.addEventListener('click', showMenu);
+closeMenu.addEventListener('click', hideMenu);
